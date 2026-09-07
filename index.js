f41508fc5197f327b37962b89cfd9c5a4e545376
Dinesh107 <dineshjr107@gmail.com>
2026-09-07 19:01:02 +0530

    20th commit

--- index.js
+++ index.js
@@ -885,3 +885,242 @@
 // function getMax(accum, element) {
 //     return Math.max(accum, element);
 // }
+
+// function declaration 
+
+// function hello() // signature part
+// {
+//     console.log("Hello siva"); // implementation part
+// }
+
+// const hello = function() {
+//     console.log("Hello siva");
+// }
+
+// hello();
+
+// setTimeout(hello, 3000);
+
+// setTimeout(function() {
+//      console.log("Hello siva");
+// }, 3000);
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// const squares = numbers.map(function(element){
+//     return Math.pow(element, 2);
+// });
+
+// const evenNums = numbers.filter(function(element){
+//     return element % 2 === 0;
+// });
+
+// console.log(squares);
+// console.log(evenNums);
+
+
+// const total = numbers.reduce(function(accum, element) {
+//     return accum + element;
+// });
+// console.log(total);
+
+// function square(element) {
+//     return Math.pow(element, 2);
+// }
+
+// function hello() {
+//     console.log("hello siva");
+// }
+
+// hello();
+
+
+// const hello = function() {
+// console.log("hello siva");
+// }
+
+// const hello = (name) => { console.log(`hello ${name}`)
+//                         console.log(`you are ${22} years old`)};
+// hello("siva", 22);
+
+
+// setTimeout(function(){
+//      console.log("hello");
+// }, 3000);
+
+// function hello() {
+//     console.log("hello");
+// }
+
+
+// setTimeout(() => console.log("hello"), 3000);
+
+
+// const numbers =  [1, 2, 3, 4, 5, 6, 7];
+
+// const squares = numbers.map((element) => Math.pow(element, 2));
+
+// const evenNums = numbers.filter((element) => element % 2 === 0);
+
+// const total = numbers.reduce((accum, element) => accum + element);
+
+// console.log(squares);
+// console.log(evenNums);
+// console.log(total);
+
+
+
+// const person1 = {
+//     firstName: "siva",
+//     lastName: "kumar",
+//     age: 22,
+//     isEmpolyed: true,
+
+//     sayHello: function(){console.log("hai i am siva");},
+//     eat: function(){console.log("i ma eating briyani")},
+
+// }
+
+// const person2 = {
+//     firstName: "naveen",
+//     lastName: "raj",
+//     age: 21,
+//     isEmpolyed: false,
+
+//     sayHello: () => console.log("I am naveen"),
+//     eat: function(){console.log("i ma eating fried rice")},
+
+// }
+
+
+
+// person1.sayHello();
+// person1.eat();
+
+// person2.sayHello();
+// person2.eat();
+
+// console.log(person1.firstName);
+// console.log(person1.lastName);
+// console.log(person1.age);
+// console.log(person1.isEmpolyed);
+// console.log(person1.isEmpolyed);
+
+// console.log(person2.firstName);
+// console.log(person2.lastName);
+// console.log(person2.age);
+// console.log(person2.isEmpolyed);
+
+
+// const person1 = {
+//     name: "siva",
+//     favFood: "curd rice",
+
+//     sayHello: function(){console.log(`hello i am ${this.name}`)},
+//     eat: function(){console.log(`${this.name} is eating ${this.favFood}`)}
+// }
+// const person2 = {
+//     name: "naveen",
+//     favFood: "sambar rice",
+
+//     sayHello: function(){console.log(`hello i am ${this.name}`)},
+//     eat: function(){console.log(`${this.name} is eating ${this.favFood}`)}
+// }
+
+// person1.sayHello();
+// person1.eat();
+// person2.sayHello();
+// person2.eat();
+
+
+// function Bike(make, model, year, color) {
+//    this.make = make;
+//    this.model = model;
+//    this.year = year;
+//    this.color = color;
+
+//    this.drive = function(){console.log(`You drive this ${this.model}`)}
+// }
+
+// const bike1 = new Bike("tvs", "apache", 2024, "red");
+// const bike2 = new Bike("yamaha", "fz", 2022, "black");
+// const bike3 = new Bike("honda", "activa", 2021, "silver");
+
+// console.log(bike1.make);
+// console.log(bike1.model);
+// console.log(bike1.year);
+// console.log(bike1.color);
+
+// console.log(bike2.make);
+// console.log(bike2.model);
+// console.log(bike2.year);
+// console.log(bike2.color);
+
+
+// console.log(bike3.make);
+// console.log(bike3.model);
+// console.log(bike3.year);
+// console.log(bike3.color);
+
+// bike1.drive();
+// bike2.drive();
+// bike3.drive();
+
+
+
+// class Product {
+
+//    constructor(name, price) {
+//     this.name = name;
+//     this.price = price;
+//    }
+
+//    displayProduct() {
+//     console.log(`Product: ${this.name}`);
+//     console.log(`Price: ₹${this.price.toFixed(2)}`);
+//    }
+
+//    salesTax(salesTax) {
+//        return this.price + (this.price * salesTax);
+//    }
+
+// }
+
+// const salesTax = 0.05;
+// // object creation or instance creation 
+// const product1 = new Product("mouse", 300);
+// const product2 = new Product("keyborad", 200.12);
+// const product3 = new Product("earphones", 320);
+
+// product1.displayProduct();
+// product2.displayProduct();
+// product3.displayProduct();
+
+// const total1 = product1.salesTax(salesTax);
+// const total2 = product2.salesTax(salesTax);
+// const total3 = product3.salesTax(salesTax);
+
+// console.log(`total prie with tax is ₹${total1.toFixed(2)}`);
+// console.log(`total prie with tax is ₹${total2.toFixed(2)}`);
+// console.log(`total prie with tax is ₹${total3.toFixed(2)}`);
+
+
+class MathUtil {
+    static PI = 3.14159;
+
+    static getDiameter(radius) {
+        return radius * 2;
+    }
+
+    static getCircumference(radius) {
+        return 2 * this.MathUtil.PI * radius;
+    }
+
+    
+
+}
+
+
+console.log(MathUtil.PI);
+console.log(MathUtil.getDiameter(10));
+console.log(MathUtil.getCircumference(20));
